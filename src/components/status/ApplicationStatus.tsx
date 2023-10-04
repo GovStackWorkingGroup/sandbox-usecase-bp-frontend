9ea904ffb4e74e3bf320f2d2b4fe8c0662ea18a9
Valentin Filyov <valentin.filyov@athlonsofia.com>
2023-10-04 13:43:34 +0300

    SND-557: Status indicator refactoring

--- src/components/status/ApplicationStatus.tsx
+++ src/components/status/ApplicationStatus.tsx
@@ -1,91 +1,79 @@
 import { Flex, Text } from "@chakra-ui/react";
 import { colors } from "../../chakra-overrides/colors";
 
-import CheckCircle from "@assets/icons/check-circle-2.svg?react";
 import AlertCircle from "@assets/icons/alert-circle.svg?react";
+import CheckCircle from "@assets/icons/check-circle-2.svg?react";
 import CircleEllipsis from "@assets/icons/circle-ellipsis.svg?react";
-import XCircle from "@assets/icons/x-circle.svg?react";
 import PauseCircle from "@assets/icons/pause-circle.svg?react";
+import XCircle from "@assets/icons/x-circle.svg?react";
 
-enum ApplicationStatus {
-  "completed" = "COMPLETED",
-  "not_started" = "NOT STARTED", 
-  "in_progress" = "IN PROGRESS", 
-  "rejected" = "REJECTED", 
-  "in_review" = "IN REVIEW", 
-  "action_needed" = "ACTION NEEDED", 
-  "draft" = "DRAFT", 
-  "approved" = "APPROVED", 
+export enum Status {
+  COMPLETED = "COMPLETED",
+  NOT_STARTED = "NOT STARTED",
+  IN_PROGRESS = "IN PROGRESS",
+  REJECTED = "REJECTED",
+  IN_REVIEW = "IN REVIEW",
+  ACTION_NEEDED = "ACTION NEEDED",
+  DRAFT = "DRAFT",
+  APPROVED = "APPROVED",
 }
 
-export function ApplicationCompleted () {
+  const statusConfig = {
+  [Status.COMPLETED]: {
+    icon: <CheckCircle fill={colors.status.green} />,
+    title: "COMPLETED",
+    color: colors.status.green
+  },
+  [Status.NOT_STARTED]: {
+    icon: <AlertCircle fill={colors.status.grey} />,
+    title: "NOT STARTED",
+    color: colors.status.grey
+  },
+  [Status.IN_PROGRESS]: {
+    icon: <CircleEllipsis fill={colors.status.blue} />,
+    title: "IN PROGRESS",
+    color: colors.status.blue
+  },
+  [Status.REJECTED]: {
+    icon: <XCircle fill={colors.status.red} />,
+    title: "REJECTED",
+    color: colors.status.red
+    },
+    [Status.IN_REVIEW]: {
+    icon: <CircleEllipsis fill={colors.status.yellow} />,
+    title: "IN REVIEW",
+    color: colors.status.yellow
+  },
+  [Status.ACTION_NEEDED]: {
+    icon: <AlertCircle fill={colors.status.red} />,
+    title: "ACTION NEEDED",
+    color: colors.status.red
+  },
+  [Status.DRAFT]: {
+    icon: <PauseCircle fill={colors.status.grey} />,
+    title: "DRAFT",
+    color: colors.status.grey
+  },
+  [Status.APPROVED]: {
+    icon: <CheckCircle fill={colors.status.green} />,
+    title: "APPROVED",
+    color: colors.status.green
+  }
+}
+
+interface ApplicationStatusProps {
+  status: Status
+}
+
+export default function ApplicationStatus(
+  {status}: ApplicationStatusProps
+  ) {
   return (
+    <>
     <Flex direction="row" p="5px">
-      <CheckCircle fill={colors.status.green}/>
-      <Text px="5px" color={colors.status.green} fontWeight="semibold">{ApplicationStatus.completed}</Text>
+      {statusConfig[status].icon}
+      <Text px="5px" color={statusConfig[status].color} fontWeight="semibold">{statusConfig[status].title}</Text>
     </Flex>
-  )
-}
-
-export function ApplicationNotStarted () {
-  return (
-    <Flex direction="row" p="5px">
-      <AlertCircle fill={colors.status.grey}/>
-      <Text px="5px" color={colors.status.grey} fontWeight="semibold">{ApplicationStatus.not_started}</Text>
-    </Flex>
-  )
-}
-
-export function ApplicationInProgress () {
-  return (
-      <Flex direction="row" p="5px">
-        <CircleEllipsis fill={colors.status.blue}/>
-        <Text px="5px" color={colors.status.blue} fontWeight="semibold">{ApplicationStatus.in_progress}</Text>
-      </Flex>
-  )
-}
-
-export function ApplicationRejected () {
-  return (
-    <Flex direction="row" p="5px">
-      <XCircle fill={colors.status.red}/>
-      <Text px="5px" color={colors.status.red} fontWeight="semibold">{ApplicationStatus.rejected}</Text>
-    </Flex>
-  )
-}
-
-export function ApplicationInReview () {
-  return (
-    <Flex direction="row" p="5px">
-      <CircleEllipsis fill={colors.status.yellow}/>
-      <Text px="5px" color={colors.status.yellow} fontWeight="semibold">{ApplicationStatus.in_review}</Text>
-  </Flex>
-  )
-}
-
-export function ApplicationActionNeeded () {
-  return (
-    <Flex direction="row" p="5px">
-      <AlertCircle fill={colors.status.red}/>
-      <Text px="5px" color={colors.status.red} fontWeight="semibold">{ApplicationStatus.action_needed}</Text>
-  </Flex>
-  )
-}
-
-export function ApplicationDraft () {
-  return (
-    <Flex direction="row" p="5px">
-      <PauseCircle fill={colors.status.grey}/>
-      <Text px="5px" color={colors.status.grey} fontWeight="semibold">{ApplicationStatus.draft}</Text>
-    </Flex>
-  )
-}
-
-export function ApplicationApproved () {
-  return (
-    <Flex direction="row" p="5px">
-      <CheckCircle fill={colors.status.green}/>
-      <Text px="5px" color={colors.status.green} fontWeight="semibold">{ApplicationStatus.approved}</Text>
-    </Flex>
+    </>
   )
 }
