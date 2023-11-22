e5a7bb57d2c33f9389cd9398b684d22e9c83a3ad
Priit Puru <priit.puru@twn.ee>
2023-11-22 11:51:44 +0200

    Add example for chakra grid usage

--- src/features/front-page/FrontPage.tsx
+++ src/features/front-page/FrontPage.tsx
@@ -1,3 +1,4 @@
+import { Grid, GridItem } from "@chakra-ui/layout";
 import Announcements from "./components/announcements/Announcements";
 import Hero from "./components/hero/Hero";
 import PopularServices from "./components/popular-services/PopularServices";
@@ -5,11 +6,27 @@ import Topics from "./components/topics/Topics";
 
 export default function FrontPage() {
   return (
-    <>
-      <Hero />
-      <PopularServices />
-      <Topics />
-      <Announcements />
-    </>
+    <Grid
+      templateAreas={{
+        sm: `"hero" "popular-services" "topics" "announcements"`,
+        md: `"hero hero"
+    "topics popular-services"
+    "topics announcements"
+    "topics announcements"`,
+      }}
+    >
+      <GridItem area={"hero"}>
+        <Hero />
+      </GridItem>
+      <GridItem area="popular-services">
+        <PopularServices />
+      </GridItem>
+      <GridItem area="topics">
+        <Topics />
+      </GridItem>
+      <GridItem area="announcements">
+        <Announcements />
+      </GridItem>
+    </Grid>
   );
 }
